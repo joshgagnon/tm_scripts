7793f5c292afddcfcd40484e9dadfba25efd6f04
Joshua Gagnon <josh.n.gagnon@gmail.com>
2016-08-26 09:57:53 +1200

    blocking more ads

--- block_fb_ads.js
+++ block_fb_ads.js
@@ -10,10 +10,28 @@
 
 (function() {
     'use strict';
+    var textMatchStrings = ['Suggested Post', 'Suggested Game'];
+
+    function textMatch(el){
+        return textMatchStrings.some(function(str){ return el.textContent === str; });
+    }
+
+    function ancestor(el, gen){
+        while(gen-- > 0){
+            el = el.parentNode;
+        }
+        return el;
+    }
+
     function clearAds() {
         [].slice.call(document.querySelectorAll('.userContentWrapper>div>div'))
-            .filter(function(el){return el.textContent==='Suggested Post';})
-            .map(function(el){return el.parentNode.parentNode.parentNode.style.display="none";});
+            .filter(function(el){return textMatch(el); })
+            .map(function(el){el.parentNode.parentNode.parentNode.style.display="none";});
+
+        [].slice.call(document.querySelectorAll('.uiStreamSponsoredLink'))
+          .map(function(el){
+              ancestor(el, 10).style.display="none";
+        });
 
     }
     var target = document.querySelector('#stream_pagelet>div:last-child');
